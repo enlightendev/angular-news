d451b67f0c6642749e3a6570665b4403815cac85
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-10-05 14:38:27 -0400

    delete post

--- app/scripts/controllers/PostsCtrl.js
+++ app/scripts/controllers/PostsCtrl.js
@@ -14,4 +14,8 @@ app.controller('PostsCtrl', function ($scope) {
         $scope.post = {url: 'http://', title: ''};
     };
 
+    $scope.deletePost = function (index) {
+        $scope.posts.splice(index, 1);
+    };
+
 });
