134af4b5fe0a9fde4adaba409bec5b688bbae8e5
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-10-06 11:58:05 -0400

    added a navigation section; moved adding post to form in nav section; added filter to display host info from a url.

---
+++ app/scripts/controllers/NavCtrl.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * $scope: model
+ * $location: used to navigate
+ * Post: post service wraps angular firebase api;s
+ */
+app.controller('NavCtrl', function($scope, $location, Post){
+
+    $scope.post = {url: 'http://', 'title': ''};
+
+    /**
+     * The firebase methods return a promise which we can then chain with .then and pass a function that will get
+     * called once the operation completes
+     */
+
+
+    $scope.submitPost = function () {
+
+        /**
+         * call angular to create a post.
+         */
+        Post.create($scope.post).then(function (ref) {
+            $scope.post = {url: 'http://', 'title': ''};
+
+            /**
+             * redirect to post page after submitting post.
+             */
+            $location.path('/posts/' + ref.name());
+        });
+    };
+
+});
--- app/scripts/controllers/PostsCtrl.js
+++ app/scripts/controllers/PostsCtrl.js
@@ -4,29 +4,6 @@ app.controller('PostsCtrl', function ($scope, $location, Post) {
 
     $scope.posts = Post.all;
 
-    $scope.post = {url: 'http://', 'title': ''};
-
-    /**
-     * The firebase methods return a promise which we can then chain with .then and pass a function that will get
-     * called once the operation completes
-     */
-
-
-    $scope.submitPost = function () {
-
-        /**
-         * call angular to create a post.
-         */
-        Post.create($scope.post).then(function (ref) {
-            $scope.post = {url: 'http://', 'title': ''};
-
-            /**
-             * redirect to post page after submitting post.
-             */
-            $location.path('/posts/' + ref.name());
-        });
-    };
-
     $scope.deletePost = function (post) {
         Post.delete(post);
     };
